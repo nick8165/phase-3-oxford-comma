586c141b1d3e748cccb638b628f4f78b24c7868a
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-06 12:22:44 -0600

    c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        string = array[0]
+        string
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+    else
+    string = ""
+    x = 0
+        while x < array.length - 1 do
+            string = string + array[x] + ", "
+            x += 1
+        end
+        string = string + "and #{array[-1]}"
+        string
+    end
 end
